41717ea54f9b54f49f81512b32d67d69a750a3b4
leishilin <80253860+leishilin@users.noreply.github.com>
2021-04-03 12:44:52 +0800

    Add files via upload

---
+++ bmp读写.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "readbmp.h"
+
+using namespace std;
+int main()
+{
+    bitmapT ts("C:\\Users\\12739\\Desktop\\ta.bmp");
+    const color white = { 255,255,255 };
+    const color black = { 0,0,0 };
+    ts.black_white();
+    cout << "success\n";
+}
